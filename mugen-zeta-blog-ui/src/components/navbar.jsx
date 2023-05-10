535f6efd7d99e9ac5bc2358b88f8e3f24b6d8a05
MugenZeta <hgarner213@gmail.com>
2023-05-09 20:02:35 -0500

    Started Nav Bar

---
+++ mugen-zeta-blog-ui/src/components/navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import {Container} from "react-bootstrap";
+import {Navbar} from "react-bootstrap";
+import {Nav} from "react-bootstrap";
+
+
+function siteNavagation(){
+    return(<Navbar>
+        <Container>
+
+        </Container>
+    </Navbar>);
+}
